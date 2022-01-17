9d9e2e62afab621d886ab794076431485ce2335d
Aryav <waryav12@gmail.com>
2022-01-17 14:45:42 -0800

    This is basic java program

---
+++ src/test/java/First1.java
@@ -0,0 +1,14 @@
+public class First1 {
+    int add(int a, int b){
+        
+        return a+b;
+
+    }
+
+    public static void main(String[] args) {
+    First1 f=new First1();
+
+        System.out.println(f.add(2,3));
+
+    }
+}
